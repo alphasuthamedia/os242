e893ce591c19ad9e56763240e1325ed8ebb3646a
alphasuthamedia <alphasuthamedia@gmail.com>
2024-09-14 20:05:40 +0700

    fixing wrong pubkey submission

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 # ####################### Replace REC2 ####
-REC2="CACD78705DE1AEF2"
+REC2="B4C990B982D2E33E"
 # ####################### ####### #### ####
 # REC1: public key
 REC1="63FB12B215403B20"
